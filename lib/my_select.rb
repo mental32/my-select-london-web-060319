bc9a7e0c1c85722ae00090b6d437fe4b151999a3
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 04:54:37 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(s)
+  seq = []
+
+  if block_given?
+
+    step = 0
+    limit = s.size
+
+    while step != limit do
+      seq << yield(s[step])
+      step += 1
+    end
+  end
+
+  seq
 end
